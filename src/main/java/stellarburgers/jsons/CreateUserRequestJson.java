ed89400e21a4561a93691a316a08795f5c970a68
Mirvari17 <mirka.agaeva@yandex.ru>
2024-09-08 16:00:59 +0300

    add: CreateUserRequestJson.java.java UserRequestJson.java.java

---
+++ src/main/java/stellarburgers/jsons/CreateUserRequestJson.java
@@ -0,0 +1,14 @@
+package stellarburgers.jsons;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserRequestJson {
+    private String email;
+    private String password;
+    private String name;
+}
